d5fa9bde8ac29f4d26c5b6da1dd5c99244aab921
rawatshubham09 <rawatshubham09@gmail.com>
2024-02-27 13:55:33 +0530

    file Push to git

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask,render_template
+import pandas as pd
+import numpy as np
 import pickle
 
 popular_df = pickle.load(open("popular.pkl","rb"))
+pt = pickle.load(open("pt.pkl","rb"))
+similarity_score = pickle.load(open("similarity_score.pkl","rb"))
+final_data = pickle.load(open("final_data.pkl","rb"))
 
 app = Flask(__name__)
 
@@ -13,6 +18,24 @@ def index():
                         image=list(popular_df['Image-URL-M'].values),
                         votes=list(popular_df['Num-Rating'].values),
                         rating=list(popular_df['Avg-Rating'].values))
+def recommend(book_name):
+    #index fetching:
+    index = np.where(pt.index==book_name)[0][0]
+    similar_items = sorted(list(enumerate(similarity_score[index])),key= lambda x:x[1], reverse=True)[1:9]
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = final_data[final_data["Book-Title"] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Title"].values))
+        item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Author"].values))
+        item.extend(list(temp_df.drop_duplicates("Book-Title")["Image-URL-M"].values))
+        data.append(item)
+        
+    return data   
+
+@app.route('/recommend')
+def recommend():
+    return render_template("recommend.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
