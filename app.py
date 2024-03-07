9f369c66117d1affd35a6621274de6900126f916
rawatshubham09 <rawatshubham09@gmail.com>
2024-03-07 09:45:53 +0530

    testing and changing little things in html file

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask,render_template, request
-import pandas as pd
 import numpy as np
 import pickle
 
@@ -21,25 +20,29 @@ def index():
 
 @app.route('/recommend')
 def recommend_ui():
-
-    book_name = ""
-    index = np.where(pt.index==book_name)[0][0]
-    similar_items = sorted(list(enumerate(similarity_score[index])),key= lambda x:x[1], reverse=True)[1:9]
-    data = []
-    for i in similar_items:
-        item = []
-        temp_df = final_data[final_data["Book-Title"] == pt.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Title"].values))
-        item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Author"].values))
-        item.extend(list(temp_df.drop_duplicates("Book-Title")["Image-URL-M"].values))
-        data.append(item)
-        
-    return render_template("recommend.html" , data = data)
+    return render_template("recommend.html")
 
 @app.route("/recommend_book", methods=["POST"])
 def recommend():
-    user_data = request.form.get("user_input")
-    return str(user_data)
+    try : 
+        user_data = request.form.get("user_input")
+        index = np.where(pt.index==user_data)[0][0]
+        similar_items = sorted(list(enumerate(similarity_score[index])),key= lambda x:x[1], reverse=True)[1:9]
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = final_data[final_data["Book-Title"] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Title"].values))
+            item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Author"].values))
+            item.extend(list(temp_df.drop_duplicates("Book-Title")["Image-URL-M"].values))
+            
+            data.append(item)
+
+        print(data)
+
+        return render_template('recommend.html', data=data)
+    except:
+        return render_template("recommend2.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
